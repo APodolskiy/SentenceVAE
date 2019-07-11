578dd4454167619d614041297bb8159fcd2df409
APodolskiy <originalea@yandex.ru>
2019-07-11 15:15:59 +0300

    Initial commit

---
+++ rvae/data_processing.py
@@ -0,0 +1,20 @@
+# Data tokenization and pre-processing
+from torch
+
+
+class PTB(Dataset):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def __len__(self):
+        pass
+
+
+class Yelp(Dataset):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def __len__(self):
+        pass
