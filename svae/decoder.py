d1f462475be2751e150a111afe48ebe6d69ab5d5
APodolskiy <originalea@yandex.ru>
2019-08-05 15:47:30 +0300

    Add RNN decoder.

--- svae/decoder.py
+++ svae/decoder.py
@@ -1,11 +1,22 @@
 import torch
 import torch.nn as nn
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
 
 class RNNDecoder(nn.Module):
-    def __init__(self):
+    def __init__(self, input_size: int = 128, hidden_size: int = 128,
+                 num_layers: int = 1, dropout: float = 0, pad_value: int = 0):
         super().__init__()
-        pass
+        self.num_layers = num_layers
+        self.pad_value = pad_value
+        self.rnn = nn.GRU(input_size=input_size,
+                          hidden_size=hidden_size,
+                          num_layers=self.num_layers,
+                          dropout=dropout,
+                          bidirectional=False)
 
-    def forward(self, x):
-        pass
+    def forward(self, x: torch.Tensor, lengths: torch.Tensor):
+        x_packed = pack_padded_sequence(x, lengths)
+        out, h_n = self.rnn(x_packed)
+        out, _ = pad_packed_sequence(out, padding_value=self.pad_value)
+        return out
