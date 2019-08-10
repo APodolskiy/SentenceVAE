439e7b9088f1e26ff655ac7dc7ebdbe63f5348ff
APodolskiy <originalea@yandex.ru>
2019-08-10 21:30:02 +0300

    Add metrics aggregation classes

---
+++ svae/utils/training.py
@@ -0,0 +1,35 @@
+class AggregateMetric:
+    def __init__(self):
+        self.total_value = 0
+
+    def __call__(self, value: float):
+        self.total_value += value
+
+    def get_metric(self, reset: bool = False) -> float:
+        total_value = self.total_value
+        if reset:
+            self.reset()
+        return total_value
+
+    def reset(self):
+        self.total_value = 0
+
+
+class AverageMetric:
+    def __init__(self):
+        self.steps = 0
+        self.total_value = 0
+
+    def __call__(self, value: float, num_steps: int):
+        self.steps += 1
+        self.total_value += value
+
+    def get_metric(self, reset: bool = False) -> float:
+        average_value = self.total_value / self.steps if self.steps > 0 else 0
+        if reset:
+            self.reset()
+        return average_value
+
+    def reset(self):
+        self.steps = 0
+        self.total_value = 0
