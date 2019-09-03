81b3583e9cc91cb553df89ef4e273f001c24d7ae
APodolskiy <originalea@yandex.ru>
2019-09-03 10:07:52 +0300

    Add start and end point to interpolation path.

--- svae/utils/interpolation.py
+++ svae/utils/interpolation.py
@@ -1,29 +1,44 @@
 import numpy as np
 
 
-def lerp(start: np.ndarray, end: np.ndarray, num_steps: int) -> np.ndarray:
+def lerp(start: np.ndarray,
+         end: np.ndarray,
+         num_steps: int,
+         startpoint: bool = True,
+         endpoint: bool = True) -> np.ndarray:
     """
     Linear interpolation between two points
     :param start: first point
     :param end: second point
     :param num_steps: number of interpolation steps
+    :param startpoint: whether to include start point
+    :param endpoint: whether to include end point
     :return: `numpy.ndarray`
     """
-    steps = np.linspace(0, 1, num_steps, endpoint=False)[1:]
+    start_idx = 0 if startpoint else 1
+    steps = np.linspace(0, 1, num_steps, endpoint=endpoint)[start_idx:]
     steps = steps.reshape(-1, 1)
     return start * (1 - steps) + end * steps
 
 
-def slerp(start: np.ndarray, end: np.ndarray, num_steps: int) -> np.ndarray:
+def slerp(start: np.ndarray,
+          end: np.ndarray,
+          num_steps: int,
+          startpoint: bool = True,
+          endpoint: bool = True
+          ) -> np.ndarray:
     """
     Spherical interpolation between two points
     :param start:
     :param end:
     :param num_steps:
+    :param startpoint: whether to include start point
+    :param endpoint: whether to include end point
     :return:
     """
+    start_idx = 0 if startpoint else 1
     omega = np.arccos((start * end).sum() / (np.linalg.norm(start) * np.linalg.norm(end)))
     sin_omega = np.sin(omega)
-    steps = np.linspace(0, 1, num_steps, endpoint=False)[1:]
+    steps = np.linspace(0, 1, num_steps, endpoint=endpoint)[start_idx:]
     steps = steps.reshape(-1, 1)
     return np.sin((1.0 - steps) * omega) / sin_omega * start + np.sin(steps * omega) / sin_omega * end
