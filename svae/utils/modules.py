e905b57e46dc92961298dc5cc0a1251ddf2db012
APodolskiy <originalea@yandex.ru>
2020-01-15 08:41:19 +0300

    Add dropout to residual block layers.

--- svae/utils/modules.py
+++ svae/utils/modules.py
@@ -8,18 +8,21 @@ class ResidualBlock(nn.Module):
                  in_channels: int = 1024,
                  residual_channels: int = 512,
                  dilation: int = 1,
-                 kernel_size: int = 3):
+                 kernel_size: int = 3,
+                 drop_prob: float = 0.1):
         super(ResidualBlock, self).__init__()
         self._pad = dilation * (kernel_size - 1)
         self.conv1 = nn.Conv1d(in_channels=in_channels,
                                out_channels=residual_channels,
                                kernel_size=1,
                                padding=0)
+        self.drop_layer_1 = nn.Dropout(p=drop_prob)
         self.conv2 = nn.Conv1d(in_channels=residual_channels,
                                out_channels=residual_channels,
                                dilation=dilation,
                                kernel_size=kernel_size,
                                padding=self._pad)
+        self.drop_layer_2 = nn.Dropout(p=drop_prob)
         self.conv3 = nn.Conv1d(in_channels=residual_channels,
                                out_channels=in_channels,
                                kernel_size=1,
@@ -27,9 +30,13 @@ class ResidualBlock(nn.Module):
 
     def forward(self, x: torch.Tensor):
         x_ = self.conv1(x).relu()
+        x_ = self.drop_layer_1(x_)
         x_ = self.conv2(x_)
         if self._pad != 0:
             x_ = x_[:, :, :-self._pad]
         x_ = x_.relu()
-        x_ = self.conv3(x_).relu()
-        return x + x_
+        x_ = self.drop_layer_2(x_)
+        x_ = self.conv3(x_)
+        repr_sum = x + x_
+        res = repr_sum.relu()
+        return res
